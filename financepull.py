d071e6a94f6bea529be14f5652369ec919305d0f
CycleMichael94 <38821582+CycleMichael94@users.noreply.github.com>
2019-02-11 12:31:58 -0800

    Add files via upload
    This is a work in progress program.

---
+++ financepull.py
@@ -0,0 +1,32 @@
+import csv
+import openpyxl
+import pandas
+import iexfinance
+from iexfinance.stocks import Stock
+
+#Unser inputs tickers and they are split
+tinput = input('Enter the stock ticker: ')
+tickers = tinput.split(' ')
+
+#Output is defined and API request for stock data is made
+company = Stock(tickers, output_format='pandas')
+
+#API request is made for specific endpoints
+info = company.get_company()
+quote = company.get_quote()
+keystat = company.get_key_stats()
+news = company.get_news()
+peers = company.get_peers()
+dividends = company.get_dividends()
+financials = company.get_financials()
+chart = company.get_chart()
+
+#Stock data is written into a single excel workbook
+with pandas.ExcelWriter('new.xlsx') as writer:
+    info.to_excel(writer, sheet_name='Info')
+    quote.to_excel(writer, sheet_name='Quote')
+    keystat.to_excel(writer, sheet_name='Key Stats')
+    news.to_excel(writer, sheet_name='News', index=False)
+    #peers.to_excel(writer, sheet_name='Peers', index=False)
+    #dividends.to_excel(writer, sheet_name='Dividends')
+    #chart.to_excel(writer, sheet_name='Chart')
